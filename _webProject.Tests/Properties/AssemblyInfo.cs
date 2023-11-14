2f7169ab36171d963153575f98fd282b7a3ba61f
ars <41697509+ArsalanMaksimov@users.noreply.github.com>
2023-11-14 12:52:01 +0300

    #1 добавлен проект для unit тестирования, там будут проводиться модульные, интеграционные, системные типы тестов (#1)
    Co-authored-by: ArsalanMaksimov <togo-linux@yandex.ru>

---
+++ _webProject.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("_webProject.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_webProject.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("39a125e4-c871-4497-967c-a661ddd3c77d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
